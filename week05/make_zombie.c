d2aa226a9d6942398abcd246c88f8821f42dbfc1
Hoto Ras <hotoras03@gmail.com>
2026-04-03 10:57:35 +0900

    5주차: 1차 커밋

---
+++ week05/make_zombie.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+int main(){
+	pid_t pid = fork(); // get forked
+	if (!pid) { // pid == 0
+		// u r child
+		printf("process %d\t exits in 3 seconds:\n",getpid());
+		sleep(3);
+		printf("child process kill\n"); _exit(0);
+	} else if (pid > 0) {
+		printf("get child %d\t from %d\t\n",pid,getpid());
+		printf("parent never stops itself\n");
+		while(1) sleep(1);
+	} else { // pid < 0
+		perror("fork: fail, exiting\n"); return 1;
+	}
+	return 0; // PROCESS_EXIT_OK
+}
